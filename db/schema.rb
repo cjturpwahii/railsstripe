1e4e404461e0f9a2b43c61b0bc64fdbcf3c6770e
Christopher Turpin <christopherturpin@Christophers-iMac.local>
2017-10-06 16:09:21 -0400

    made reciept more secure

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171006161032) do
+ActiveRecord::Schema.define(version: 20171006200553) do
 
   create_table "purchases", force: :cascade do |t|
     t.string "email"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20171006161032) do
     t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uuid"
   end
 
 end
